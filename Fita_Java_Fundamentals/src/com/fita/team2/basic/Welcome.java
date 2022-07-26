5d1a11f1b0fa345d642febeac84d213f0cf9e4ea
SHYAMSANTHOSH <89693199+SHYAMSANTHOSH@users.noreply.github.com>
2022-07-26 09:43:21 +0530

    Add files via upload

---
+++ Fita_Java_Fundamentals/src/com/fita/team2/basic/Welcome.java
@@ -0,0 +1,19 @@
+package com.fita.team2.basic;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Fita Java Programs");
+		/*
+		 * Welcome wel = new Welcome(); int value = wel.display();
+		 * System.out.println(value);
+		 */
+	}
+
+	/*
+	 * public int display() { System.out.println("Hello");
+	 * 
+	 * return 5; }
+	 */
+
+}
