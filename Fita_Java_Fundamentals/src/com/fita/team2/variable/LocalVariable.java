6fe3f7fd706120296d69f139aea30fbbbc8180c0
SHYAMSANTHOSH <89693199+SHYAMSANTHOSH@users.noreply.github.com>
2022-07-27 09:50:13 +0530

    Add files via upload

---
+++ Fita_Java_Fundamentals/src/com/fita/team2/variable/LocalVariable.java
@@ -0,0 +1,19 @@
+package com.fita.team2.variable;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalVariable loc = new LocalVariable();
+		loc.display();
+		int mark = 35;
+		System.out.println(mark);
+	}
+	
+	public void display() {
+		int value = 100;
+		System.out.print(value);
+	}
+
+}
