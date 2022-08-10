9e06c66af623570e369a48262b3714486ec57f81
SHYAMSANTHOSH <89693199+SHYAMSANTHOSH@users.noreply.github.com>
2022-08-10 09:18:20 +0530

    Add files via upload

---
+++ Fita_Java_Fundamentals/src/com/fita/team2/typecasting/HigherToLowerConversion.java
@@ -0,0 +1,14 @@
+package com.fita.team2.typecasting;
+
+public class HigherToLowerConversion {
+
+	public static void main(String[] args) {
+		double d = 100.0;
+		float f = (float) d;
+		byte b = (byte) f;
+		System.out.println(f);
+		System.out.println(b);
+
+	}
+
+}
